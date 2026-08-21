20726a9462f966434af634136c426afcea664073
FoggyCaligo <136309172+FoggyCaligo@users.noreply.github.com>
2026-08-22 02:09:49 +0900

    Use thread-local graph repository at runtime boundary

--- mai/graph/__init__.py
+++ mai/graph/__init__.py
@@ -1,6 +1,7 @@
 from .discovery import GraphDiscoveryService
 from .recall import GraphRecallService
-from .repository import GraphRepository, GraphScopeError
+from .repository import GraphScopeError
+from .thread_local_repository import GraphRepository
 
 __all__ = [
     "GraphDiscoveryService",
