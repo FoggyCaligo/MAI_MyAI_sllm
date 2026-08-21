bf58803b8cbe9805021d5fe05fe18ffdf18a34ba
FoggyCaligo <136309172+FoggyCaligo@users.noreply.github.com>
2026-08-22 00:02:22 +0900

    Reduce server console noise

--- run_server.py
+++ run_server.py
@@ -6,4 +6,11 @@ from mai.config import settings
 
 
 if __name__ == "__main__":
-    uvicorn.run("mai.server:app", host=settings.server_host, port=settings.server_port, reload=False)
+    uvicorn.run(
+        "mai.server:app",
+        host=settings.server_host,
+        port=settings.server_port,
+        reload=False,
+        access_log=False,
+        log_level="warning",
+    )
