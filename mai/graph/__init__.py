ca9b655a6c4ca366260c914ef8cb58f63792af83
FoggyCaligo <136309172+FoggyCaligo@users.noreply.github.com>
2026-08-22 19:11:09 +0900

    Export only current graph memory interfaces

--- mai/graph/__init__.py
+++ mai/graph/__init__.py
@@ -1,13 +1,9 @@
-from .discovery import GraphDiscoveryService
-from .recall import GraphRecallService
 from .repository import GraphConflictError, GraphScopeError
 from .source_store import GraphSourceStore, SourceRecord
 from .thread_local_repository import GraphRepository
 
 __all__ = [
     "GraphConflictError",
-    "GraphDiscoveryService",
-    "GraphRecallService",
     "GraphRepository",
     "GraphScopeError",
     "GraphSourceStore",
