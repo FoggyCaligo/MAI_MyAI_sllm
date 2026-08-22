c0e5d7c5cd4c85061eed2d0bcf66f26d7a38137d
FoggyCaligo <136309172+FoggyCaligo@users.noreply.github.com>
2026-08-22 18:48:39 +0900

    Remove legacy graph discovery exports

--- mai/graph/__init__.py
+++ mai/graph/__init__.py
@@ -1,12 +1,8 @@
-from .discovery import GraphDiscoveryService
-from .recall import GraphRecallService
 from .repository import GraphScopeError
 from .source_store import GraphSourceStore, SourceRecord
 from .thread_local_repository import GraphRepository
 
 __all__ = [
-    "GraphDiscoveryService",
-    "GraphRecallService",
     "GraphRepository",
     "GraphScopeError",
     "GraphSourceStore",
