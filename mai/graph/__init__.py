7c4114605d0412f6fb91488690f9a729acc22f9b
FoggyCaligo <136309172+FoggyCaligo@users.noreply.github.com>
2026-08-22 18:46:15 +0900

    Export new graph repository contracts

--- mai/graph/__init__.py
+++ mai/graph/__init__.py
@@ -1,10 +1,11 @@
 from .discovery import GraphDiscoveryService
 from .recall import GraphRecallService
-from .repository import GraphScopeError
+from .repository import GraphConflictError, GraphScopeError
 from .source_store import GraphSourceStore, SourceRecord
 from .thread_local_repository import GraphRepository
 
 __all__ = [
+    "GraphConflictError",
     "GraphDiscoveryService",
     "GraphRecallService",
     "GraphRepository",
