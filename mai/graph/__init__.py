fc8ea99e05ec60ec8488e19a8c09fa0751b9bf58
FoggyCaligo <136309172+FoggyCaligo@users.noreply.github.com>
2026-08-22 11:29:37 +0900

    Export graph source provenance store

--- mai/graph/__init__.py
+++ mai/graph/__init__.py
@@ -1,6 +1,7 @@
 from .discovery import GraphDiscoveryService
 from .recall import GraphRecallService
 from .repository import GraphScopeError
+from .source_store import GraphSourceStore, SourceRecord
 from .thread_local_repository import GraphRepository
 
 __all__ = [
@@ -8,4 +9,6 @@ __all__ = [
     "GraphRecallService",
     "GraphRepository",
     "GraphScopeError",
+    "GraphSourceStore",
+    "SourceRecord",
 ]
